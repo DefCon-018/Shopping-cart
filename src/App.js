4d37d848347fad122aa64249e56cc5c99405f640
DefCon-018 <hp761570@gmil.com>
2021-03-12 11:29:00 +0530

    Add total Price

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ class App extends React.Component {
             title: 'Mobile phone',
             price: 10000,
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1567581935884-3349723552ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
             id: 1
         },
         {
             title: 'Watch',
             price: 100,
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1461141346587-763ab02bced9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1248&q=80',
             id: 2
         },
         {
             title: 'Laptop',
             price: 99999,
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
             id: 3
         },
       ]
@@ -67,6 +67,16 @@ class App extends React.Component {
     })
     return cnt;
   }
+
+  getTotalPrice(){
+    const {products} = this.state;
+    let total = 0;
+    products.forEach((product) =>{
+      total += product.qty * product.price
+    })
+    return total;
+  }
+
   render(){
     const {products} = this.state;
     return (
@@ -77,6 +87,9 @@ class App extends React.Component {
               onDecreaseQty = {this.handleDecreaseQuantity}
               onDeleteProduct = {this.handleDeleteProduct}          
         />
+        <div style= {{margin: '10px'}}>
+          <span style= {{fontWeight: 700, fontSize: '1.3rem'}}>TOTAL</span> : <span style= {{fontWeight: 500, fontSize: '1.3rem'}}>{this.getTotalPrice()}</span>
+        </div>
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -34,7 +34,7 @@ class CartItem extends React.Component{
         return(
             <div className= "cart-item">
                 <div className= "left-block">
-                    <img style={styles.image}/>
+                    <img style={styles.image} src= {product.img}/>
                 </div>
                 <div className= "right-block">
                     <div className= "details">
